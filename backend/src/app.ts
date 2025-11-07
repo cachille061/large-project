9f765eaa355d20409b03d14f8069f0d0c0e6427c
Micah <micah.ramirez89@gmail.comgit config --global user.name Micahgit statushelpgit config --global user.email micah.ramirez89@gmail.com>
2025-11-07 17:12:22 -0500

    Make it so you can use flutter with the backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,13 +10,34 @@ import orderRoutes from "./routes/orderRoutes";
 
 export const app = express();
 
+const useFlutterCors = process.env.USE_FLUTTER_CORS === 'true';
 app.use(helmet());
-app.use(
+if (useFlutterCors) {
+  app.use(
     cors({
-        origin: process.env.FRONTEND_URL || "http://localhost:5173",
-        credentials: true,
+      origin: (origin, callback) => {
+        if (
+          !origin ||
+          /^http:\/\/localhost(:\d+)?$/.test(origin) ||
+          /^http:\/\/127\.0\.0\.1(:\d+)?$/.test(origin)
+        ) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true,
     })
-);
+  );
+}
+else {
+  app.use(
+    cors({
+      origin: process.env.FRONTEND_URL || "http://localhost:5173",
+      credentials: true,
+    })
+  );
+}
 
 // DON'T use express.json() before Better Auth handler!
 // It will cause the client API to get stuck on "pending"
